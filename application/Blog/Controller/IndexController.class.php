a439f555ad21a4d23c9ae86c58fa666bc428a71c
zst111 <35945324+zst111@users.noreply.github.com>
2018-02-04 10:32:20 +0800

    test1

---
+++ application/Blog/Controller/IndexController.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace Blog\Controller;
+
+use Common\Controller\HomebaseController;
+
+class IndexController extends HomebaseController{
+
+    
+    public function index(){
+      echo "this is blog index !";
+    }
+}
