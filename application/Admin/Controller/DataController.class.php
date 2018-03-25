04904b3a57e2de5036a847da3215ea43117fc17a
zst111 <35945324+zst111@users.noreply.github.com>
2018-03-25 14:42:17 +0800

    修改data_check/index

--- application/Admin/Controller/DataController.class.php
+++ application/Admin/Controller/DataController.class.php
@@ -78,10 +78,56 @@ class DataController extends AdminbaseController {
 
 
     /**
-     * 数据概况
+     * 数据查看
      */
 
     public function data_check(){
+        $p_id = session('p_id');
+        if($p_id ==1  ){ // 说明是mcc账户
+            // 查出该mcc账户下的所有子账户数据
+            $data = M("users")->where('p_id = '.session('ADMIN_ID'))->select();
+            $result=array();
+            foreach ($data as $key => $value) {
+                $result[$value['trade']][$value['companyname']][]=$value;
+            }
+            $this->assign('trade',$result);
+        }else{
+            // 子账户
+            $result = M("users")->where(array('id'=>session(ADMIN_ID)))->select();
+            $this->assign('user',$result);
+        }
+        $target = $_POST['target'] ?$_POST['target']:0;
+        if($target){
+            session('username_normal',$target);
+        }
+        $t = time();
+        $start = mktime(0,0,0,date("m",$t),date("d",$t),date("Y",$t)); // 当天0时0分
+        $end = mktime(23,59,59,date("m",$t),date("d",$t),date("Y",$t)); // 当天23时59分
+// 设备默认为0，全部
+        $device = $_POST['device'] ? $_POST['device'] : 0;
+        if($device){
+            session("device",$device);
+            $device = session("device");
+        }
+
+        $datepick = explode(" ",  $_POST['datepicker']);
+
+        $startDate=$datepick[0];
+
+
+        $endDate =$datepick[2];
+        $startDate = $startDate ? $startDate : date('Y-m-d',$start);
+        $endDate = $endDate ? $endDate : date('Y-m-d',$end);
+
+        //分时段 默认为5,分日
+        $unitOfTime = $_POST['unit_data'] ? $_POST['unit_data'] : 5;
+        if($unitOfTime){
+            session("unit_data",$unitOfTime);
+            $unitOfTime=session("unit_data");
+        }
+        echo $unitOfTime;
+        $param = array("startDate"=>$startDate,"endDate"=>$endDate,"platform"=>0,"Device"=>$device);
+
 
         $this->display();
     }
